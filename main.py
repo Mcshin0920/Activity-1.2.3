e44710e2fb804a196b42cf9f6fcc8b9b0e6a9f04
Mcshin0920 <90926649+Mcshin0920@users.noreply.github.com>
2021-11-17 11:22:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle
+import random
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -9,14 +10,33 @@ wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = turtle.Turtle()
+apple2 = turtle.Turtle()
+apple3 = turtle.Turtle()
+apple4 = turtle.Turtle()
+apple5 = turtle.Turtle()
 drawer = turtle.Turtle()
-
+drawer.hideturtle()
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = []
+num_of_apples = 0
+n = 0
+apple.speed(0)
+drawer.speed(0)
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
+
 def draw_apple(active_apple):
+  global num_of_apples
+  x = random.randint(-250, 250)
+  y = random.randint(-150, 150)
   active_apple.shape(apple_image)
-  draw_an_A()
-  
+  active_apple.up()
+  active_apple.goto(x, y)
+  active_apple.down()
+  draw_an_letter(x, y)
+
+  num_of_apples = num_of_apples + 1
+
 def drop_apple():
   apple.up()
   drawer.hideturtle()
@@ -24,16 +44,49 @@ def drop_apple():
   apple.goto(0,-160)
   apple.down()
 
-def draw_an_A():
+def randomlettergen():
+  num = random.randint(0, 25)
+  return num
+
+def draw_an_letter(x, y):
+  randomletter = randomlettergen()
   drawer.up()
-  drawer.goto(-25,-33)
+  drawer.goto(x-18,y-40)
   drawer.down()
   drawer.color("white")
-  drawer.write("A", font=("Arial", 40, "bold")) 
+  drawer.write(letters[randomletter], font=("Arial", 40, "bold"))
+  numbers.append(randomletter)
 
 #-----function calls-----
 
 draw_apple(apple)
-wn.onkeypress(drop_apple, "a")
-wn.listen()
+num = str(numbers[n])
+num = int(num)
+wn.onkeypress(drop_apple, letters[num])
+n = n + 1
+
+draw_apple(apple2)
+num = str(numbers[n])
+num = int(num)
+wn.onkeypress(drop_apple, letters[num])
+n = n + 1
+
+draw_apple(apple3)
+num = str(numbers[n])
+num = int(num)
+wn.onkeypress(drop_apple, letters[num])
+n = n + 1
+
+draw_apple(apple4)
+num = str(numbers[n])
+num = int(num)
+wn.onkeypress(drop_apple, letters[num])
+n = n + 1
+
+draw_apple(apple5)
+num = str(numbers[n])
+num = int(num)
+wn.onkeypress(drop_apple, letters[num])
+n = n + 1
+
 wn.mainloop()
